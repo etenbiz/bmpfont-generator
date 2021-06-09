9cb748697425fe720778fab33cfde2eedc3536ce
Janvier Peng <janvier@yeah.net>
2021-06-09 14:32:45 +0800

    Code completion and tests passed.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# bmpfont-generator
-Bitmap-Font / Dot-Matrix Font Generator
+# Bitmap-Font / Dot-Matrix Font Generator 位图字体产生器
+
+位图字型产生器，用于从点阵字库里抽取特定文字的点阵信息后，存到一个较小的文件中，适合嵌入式开发使用。
+
+环境依赖：
+
+需要安装 python3 和 numpy
+
+使用方式：
+
+```
+$ python3 bmpfnt-gen.py [-h] [-f FONT_FILE] [-i INPUT] [-o OUTPUT]
+
+参数:
+  -h, --help            显示帮助
+  -f FONT_FILE, --font-file FONT_FILE
+                        点阵字体文件。默认值：fonts/HZK12
+  -i INPUT, --input INPUT
+                        包含所需文字的输入文件。默认值：input.txt
+  -o OUTPUT, --output OUTPUT
+                        输出的点阵文件。默认值: hzk12.fnt
+```
+
+由于涉及版权问题，本仓库并不包含存放点阵字型文件的 *fonts* 目录。大家可以到以下第三方链接进行下载、解压缩后使用。
+
+点阵字库[下载](https://pan.baidu.com/s/1xFEAaoPXqvc5q-mo1q8UaA)，网盘提取码：n3wr
---
+++ bmpfnt-gen.py
@@ -0,0 +1,101 @@
+import numpy as np
+import os
+import re
+import sys
+import argparse
+
+def get_bmp_font(bmpfnt, bmpsym, single_char, font_height):
+    result = b''
+    if len(single_char) == 2 and type(single_char) == bytes:
+        if font_height == 12:
+            font_length = (font_height + 4) // 8 * font_height
+        else:
+            font_length = font_height // 8 * font_height
+        area = single_char[0] - 0xA0
+        index = single_char[1] - 0xA0
+        if font_height in [24, 40, 48]:
+            if area - 1 >= 15:
+                offset = (94 * (area - 1 - 15) + (index - 1)) * font_length
+                result = bmpfnt[offset : offset + font_length]
+            else:
+                offset = (94 * (area - 1) + (index - 1)) * font_length
+                result = bmpsym[offset : offset + font_length]
+        else:
+            offset = (94 * (area - 1) + (index - 1)) * font_length
+            result = bmpfnt[offset : offset + font_length]
+        if font_height == 24:
+            result = np.packbits(
+                    np.flip(
+                        np.rot90(
+                            np.reshape(
+                                np.unpackbits(
+                                    np.frombuffer(result, dtype=np.dtype('B'))
+                                ),
+                                (font_height, font_height)
+                            ),
+                            k=3
+                        ),
+                        axis=1
+                    ).flatten()
+                ).tobytes()
+    return result
+
+def main():
+
+    # parsing arguments
+    parser = argparse.ArgumentParser(description='To generate bitmap font file for specific simplified Chinese characters. (GB2312 only)')
+    parser.add_argument('-f', '--font-file', required=False, help="the font file. default: fonts/HZK12")
+    parser.add_argument('-i', '--input', required=False, help="the input file which contains all the needed characters. default: input.txt")
+    parser.add_argument('-o', '--output', required=False, help="the output file of bitmap font. default: hzk12.fnt")
+    args = parser.parse_args()
+
+    # check if the font file is exist
+    font_file = "fonts/HZK12" if not args.font_file else args.font_file
+    m = re.match('^.*HZK([\d]+)[FHKS]?$', font_file)
+    if not m or not os.access(font_file, os.R_OK):
+        print("Invalid font file.")
+        sys.exit(2)
+    else:
+        font_bits = int(m.group(1))
+
+    # init the default values
+    input_file = "input.txt" if not args.input else args.input
+    output_bin = "dist/hzk{}.fnt".format(font_bits) if not args.output else args.output
+    output_txt = "dist/hzk{}.txt".format(font_bits) if not args.output else args.output + ".txt"
+
+    # read inputs from file
+    with open(input_file, "r") as fin:
+        chars_raw = fin.read().strip()
+        chars_utf8 = ''. join(sorted(set(chars_raw), key=chars_raw.index))
+        chars_gb2312 = chars_utf8.encode("gb2312")
+
+    # read the font file
+    with open(font_file, "rb") as ffnt:
+        bmpfnt = ffnt.read()
+
+    # read the symbol file if necessary
+    bmpsym = None
+    if font_bits in [24, 40, 48]:
+        with open(font_file[:-1] + "T", "rb") as ffnt:
+            bmpsym = ffnt.read()
+
+    # generate the bitmap font file which is just contains the input characters.
+    with open(output_txt, "w") as fout_txt:
+        fout_txt.write("       0 1 2 3 4 5 6 7 8 9 a b c d e f\n")
+        with open(output_bin, "wb") as fout_bin:
+            for i in range(0, len(chars_gb2312)//2):
+                fout_bin.write(get_bmp_font(bmpfnt, bmpsym, chars_gb2312[i*2:i*2+2], font_height=font_bits))
+                if i % 16 == 0:
+                    fout_txt.write("0x%04x " % i)
+                fout_txt.write(chars_utf8[i])
+                if (i + 1) % 16 == 0:
+                    fout_txt.write("\n")
+        fout_txt.write("\n")
+    
+    # print the result of characters map and output file
+    with open(output_txt, "r") as fin_txt:
+        print(fin_txt.read())
+    print("Output bin: {}, map file: {}".format(output_bin, output_txt))
+
+if __name__ == '__main__':
+    main()
